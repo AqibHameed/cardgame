f4ce5454ffa9fb95d44dba2705d4034c21848da3
Muhammad Umair Raza <umair.raza101@gmail.com>
2018-09-17 17:57:37 +0500

    change email to username

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   devise_for :users,  controllers: {
-      passwords: 'users/passwords'
+      passwords: 'users/passwords',
+      sessions: 'users/sessions'
+
   }
   as :user do
     get 'users/password/update_password', to: 'users/passwords#update_password'
@@ -10,5 +16,7 @@ Rails.application.routes.draw do
  # get 'users/update_password', to: 'users/passwords#update_password'
   get 'game/index'
   root 'home#index'
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 
 end
