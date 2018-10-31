07a9f5a589c8b38ddd64275078f7bd0e4887e1fb
Muhammad Umair Raza <umair.raza101@gmail.com>
2018-10-31 12:44:05 +0500

    everything is working now mails, payment, creating user

--- app/models/subcription.rb
+++ app/models/subcription.rb
@@ -7,13 +7,17 @@ class Subcription < ApplicationRecord
         cmd: '_xclick',
         upload: 1,
         return: "#{ENV['app_host']}#{return_path}",
-        invoice: id,
-        amount: self.amount,
+        invoice: "#{id}test",
+        amount: self.amount/self.totalgames,
         item_name: self.package_name,
         item_number: self.package_plan_id,
-        quantity: '1',
+        quantity: self.totalgames,
         notify_url: "#{ENV['app_host']}/hook"
     }
     "#{ENV['paypal_host']}/cgi-bin/webscr?" + values.to_query
   end
+
+
+
+
 end
